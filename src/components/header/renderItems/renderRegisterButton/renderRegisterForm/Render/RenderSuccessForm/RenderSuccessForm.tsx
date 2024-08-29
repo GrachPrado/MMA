ee8d77e2dc3893ee9969b5a947ef5eb558aac821
Bohdan Grachov <bgrachov.94@gmail.com>
2024-08-29 15:30:51 +0300

    renderSuccessForm

---
+++ src/components/header/renderItems/renderRegisterButton/renderRegisterForm/Render/RenderSuccessForm/RenderSuccessForm.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import "./renderSuccessForm.scss";
+
+const RenderSuccessForm: React.FC = () => {
+
+  return (
+    <div className='registerSuccessForm'>
+      <div className='registerSuccessForm__container'>
+        zhopa
+      </div>
+    </div>
+  );
+}
+
+export default RenderSuccessForm;
